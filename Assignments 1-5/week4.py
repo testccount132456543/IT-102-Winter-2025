575cba18cd0dbe002faa939bea426215d241ff56
testccount132456543 <spoiler.hollow_1u@icloud.com>
2025-01-19 19:51:13 -0800

    Added a function

--- Assignments 1-5/week4.py
+++ Assignments 1-5/week4.py
@@ -1,12 +1,27 @@
+"""
+This script asks the user if today is a good day and includes a 
+function to print through a iteration x - 10
+"""
 
+def send_message():
+    """
+    Prints a positive message and says yeah it is 10 times
+    """
+    for _ in range(10):
+        print("Yeah it is!")
 
 # Ask user if today is good y/n
 response = input("Is today a good day?: y/n ").lower()
+
+
 if response == 'y':
-    for _ in range(10):
-        print("Yeah it is!")
+    # Call function to print
+    send_message()
+
 elif response == 'n':
     print("Maybe tomorrow will be better")
+
+
 else:
     print("Please reinput to ensure that I have the correct response.")
     
