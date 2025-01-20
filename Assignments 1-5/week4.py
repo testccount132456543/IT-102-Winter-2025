050d56ec3c22a90a24ac5ebf519713a481f83851
testccount132456543 <spoiler.hollow_1u@icloud.com>
2025-01-19 19:40:11 -0800

    Added files

---
+++ Assignments 1-5/week4.py
@@ -0,0 +1,12 @@
+
+
+# Ask user if today is good y/n
+response = input("Is today a good day?: y/n ").lower()
+if response == 'y':
+    for _ in range(10):
+        print("Yeah it is!")
+elif response == 'n':
+    print("Maybe tomorrow will be better")
+else:
+    print("Please reinput to ensure that I have the correct response.")
+    
